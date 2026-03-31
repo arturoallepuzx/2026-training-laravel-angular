c815a5a2e1d2409292e03867159fab629f3c4cfe
arturoallepuzx <arturoallepuzx@gmail.com>
2026-03-31 10:34:32 +0200

    user migration refactored

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,12 +12,19 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->uuid('uuid')->unique();
+            $table->foreignId('restaurant_id')->nullable()->constrained('restaurants');
+            $table->string('role')->default('operator');
+            $table->string('image_src')->nullable();
             $table->string('name');
+
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('pin')->nullable();
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
@@ -47,3 +53,4 @@ return new class extends Migration
         Schema::dropIfExists('sessions');
     }
 };
+
