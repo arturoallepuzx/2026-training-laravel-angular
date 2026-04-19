6919b0c3fc31c9b4b70d264f45d5f91f74cb0582
arturoallepuzx <arturoallepuzx@gmail.com>
2026-04-19 02:03:52 +0200

    refactor: simplify UserFactory by removing unused properties and methods

--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -14,16 +14,8 @@ class UserFactory extends Factory
 {
     protected $model = EloquentUser::class;
 
-    /**
-     * The current password being used by the factory.
-     */
-    protected static ?string $password;
+    protected static ?string $password = null;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
@@ -33,20 +25,8 @@ class UserFactory extends Factory
             'image_src' => null,
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'pin' => fake()->numerify('####'),
-            'remember_token' => Str::random(10),
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
     }
 }
