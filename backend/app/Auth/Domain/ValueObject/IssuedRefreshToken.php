3c55abecfa365fb54404d9e0e4fa102000992aa3
arturoallepuzx <arturoallepuzx@gmail.com>
2026-04-21 12:52:04 +0200

    feat: add IssuedRefreshToken value object for managing issued refresh tokens

---
+++ backend/app/Auth/Domain/ValueObject/IssuedRefreshToken.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Auth\Domain\ValueObject;
+
+use App\Auth\Domain\Entity\RefreshToken;
+
+class IssuedRefreshToken
+{
+    private RefreshToken $entity;
+
+    private RefreshTokenSecret $secret;
+
+    private function __construct(RefreshToken $entity, RefreshTokenSecret $secret)
+    {
+        $this->entity = $entity;
+        $this->secret = $secret;
+    }
+
+    public static function create(RefreshToken $entity, RefreshTokenSecret $secret): self
+    {
+        return new self($entity, $secret);
+    }
+
+    public function entity(): RefreshToken
+    {
+        return $this->entity;
+    }
+
+    public function secret(): RefreshTokenSecret
+    {
+        return $this->secret;
+    }
+}
