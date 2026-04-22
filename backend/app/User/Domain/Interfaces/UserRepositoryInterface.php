9b70e94919ff4decd35501c51bffa2175dd5faf3
arturoallepuzx <arturoallepuzx@gmail.com>
2026-04-22 16:36:02 +0200

    feat: add findByEmail method to UserRepositoryInterface for user retrieval by email

--- backend/app/User/Domain/Interfaces/UserRepositoryInterface.php
+++ backend/app/User/Domain/Interfaces/UserRepositoryInterface.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\User\Domain\Interfaces;
 
+use App\Shared\Domain\ValueObject\Email;
 use App\Shared\Domain\ValueObject\Uuid;
 use App\User\Domain\Entity\User;
 
@@ -12,4 +13,6 @@ interface UserRepositoryInterface
     public function create(User $user): void;
 
     public function findById(Uuid $id, Uuid $restaurantId): ?User;
+
+    public function findByEmail(Email $email, Uuid $restaurantId): ?User;
 }
