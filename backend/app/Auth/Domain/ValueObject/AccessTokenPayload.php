6a84e589118b3c5f06f5845d376a12ab2bd8954a
arturoallepuzx <arturoallepuzx@gmail.com>
2026-04-23 18:13:29 +0200

    fix: reorder Uuid import in AccessTokenPayload for consistency

--- backend/app/Auth/Domain/ValueObject/AccessTokenPayload.php
+++ backend/app/Auth/Domain/ValueObject/AccessTokenPayload.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace App\Auth\Domain\ValueObject;
 
 use App\Shared\Domain\ValueObject\DomainDateTime;
-use App\Shared\Domain\ValueObject\Uuid;
 use App\Shared\Domain\ValueObject\UserRole;
+use App\Shared\Domain\ValueObject\Uuid;
 
 class AccessTokenPayload
 {
